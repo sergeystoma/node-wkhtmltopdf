81146bf8d6fc689e571a1f0daa433fdc7c22bd4d
Sergey Stoma <sergey@underclouds.com>
2017-10-09 11:06:39 -0400

    Allowing exit code 1

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ function wkhtmltopdf(input, options, callback) {
 
   // call the callback with null error when the process exits successfully
   child.on('exit', function(code) {
-    if (code !== 0) {
+    if (code !== 0 && code !== 1) {
       stderrMessages.push('wkhtmltopdf exited with code ' + code);
       handleError(stderrMessages);
     } else if (callback) {
